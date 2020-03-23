0ae4aa9b2f7652c0ceff5303393db2c19102f2e6
Damian Połoch <damian@poloch.pl>
2020-03-23 14:56:15 +0100

    Init Project imgFighter

---
+++ src/main/java/pl/poloch/imgfighter/ImgfighterApplication.java
@@ -0,0 +1,13 @@
+package pl.poloch.imgfighter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgfighterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgfighterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/poloch/imgfighter/ImgfighterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.poloch.imgfighter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgfighterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
