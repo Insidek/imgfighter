49f469a3b86d5d896ea46d5627c172ce1b3e4824
Damian Połoch <damian@poloch.pl>
2020-04-03 17:31:26 +0200

    Implemetation Convert function

---
+++ src/main/java/pl/poloch/imgfighter/api/priv/ConvertImages.java
@@ -0,0 +1,40 @@
+package pl.poloch.imgfighter.api.priv;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pl.poloch.imgfighter.controllers.general.JWTController;
+import pl.poloch.imgfighter.controllers.images.ConvertImagesController;
+import pl.poloch.imgfighter.controllers.images.ImagesController;
+
+import java.io.IOException;
+
+
+@RestController
+public class ConvertImages {
+    @GetMapping(value="/api/private/img/convert", produces = "image/png")
+    public ResponseEntity<byte[]> convert(@RequestHeader("Authorization") String token, @RequestParam(required = false) String format, String bgcolor, @RequestParam(required = true) String url) throws IOException, NoSuchFieldException, IllegalAccessException {
+        JWTController.verifyJWTToken(token);
+        // IF only url is not NULL
+        if (bgcolor == null && format == null) {
+            return ImagesController.uploadImage(new ConvertImagesController(ImagesController.downloadImage(url)).convert());
+        }
+        // IF url and bgcolor is not NULL
+        if (bgcolor != null && format == null) {
+            return ImagesController.uploadImage(new ConvertImagesController(ImagesController.downloadImage(url), bgcolor).convert());
+        }
+        // IF url and format is not NULL
+        if (bgcolor == null && format != null) {
+            return ImagesController.uploadImage(new ConvertImagesController(ImagesController.downloadImage(url), format).convert());
+        }
+        // IF url and bgcolor and format is not NULL
+        if  (bgcolor != null && format != null) {
+            return ImagesController.uploadImage(new ConvertImagesController(ImagesController.downloadImage(url), format, bgcolor).convert());
+        }
+
+
+        throw new RuntimeException("This shouldn't happen!");
+    }
+}
--- src/main/java/pl/poloch/imgfighter/api/pub/ServerTime.java
+++ src/main/java/pl/poloch/imgfighter/api/pub/ServerTime.java
@@ -2,7 +2,6 @@ package pl.poloch.imgfighter.api.pub;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.poloch.imgfighter.ImgFighterApplication;
 import pl.poloch.imgfighter.controllers.TimeController;
 
 
